4e1dcf5c84e260d63c2345ecf9d93ef052fcb699
RovinaLucas <161614206+RovinaLucas@users.noreply.github.com>
2024-06-12 15:00:37 -0300

    Add files via upload

---
+++ kaatia.py
@@ -0,0 +1,17 @@
+import stramlit as st
+import speech_recognition as sr
+
+def principal():
+  st.title("Transcrição de voz")
+  upload = st.file_uploader("Faça o upload do arquivo de audio", type=["wav"])
+  if upload is not None:
+    transcrever(upload)
+
+def transcrever(upload):
+  recognizer = sr.Recognizer()
+  with sr.AudioFile(upload) as source:
+    st.write("Transcrevendo...")
+    audio = recognizer.record(source)
+    texto = recognizer.recognize_google(audio, language="pt-BR")
+    st.write(texto)
+principal()
